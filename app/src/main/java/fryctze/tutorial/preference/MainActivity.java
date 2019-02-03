5e5eb8cacbf6fc5f7273a0488a8928f4a0ed1ec2
Fikry Farenza <fryzexultant@gmail.com>
2019-02-03 15:12:31 +0700

    :sparkles: Show logged in user

--- app/src/main/java/fryctze/tutorial/preference/MainActivity.java
+++ app/src/main/java/fryctze/tutorial/preference/MainActivity.java
@@ -1,7 +1,10 @@
 package fryctze.tutorial.preference;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Deklarasi dan Menginisialisasi variable nama dengan Label Nama dari Layout MainActivity */
+        TextView nama = findViewById(R.id.tv_namaMain);
+
+        /* Men-set Label Nama dengan data User sedang login dari Preferences */
+        nama.setText(Preferences.getLoggedInUser(getBaseContext()));
+
+        /* Men-set Status dan User yang sedang login menjadi default atau kosong di
+         * Data Preferences. Kemudian menuju ke LoginActivity*/
+        findViewById(R.id.button_logoutMain).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Menghapus Status login dan kembali ke Login Activity
+                Preferences.clearLoggedInUser(getBaseContext());
+                startActivity(new Intent(getBaseContext(),LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
